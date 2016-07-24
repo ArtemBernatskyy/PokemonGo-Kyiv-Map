fddc658eea2f8c48c94945ff1e8ec5ea41a6c28e
artembernatskyy <artem.bernatskyy@gmail.com>
2016-07-24 04:41:53 +0300

    ending working day)

--- README.md
+++ README.md
@@ -2,3 +2,8 @@
 
 ### What is ready
 - getting Kyiv territory and dividing it in sectors
+- Database models are set up completely
+- Redis is caching, Celery is working too
+
+### To Do
+- all celery processes must send callback to one Redis variable
--- code/mapview/tasks.py
+++ code/mapview/tasks.py
@@ -1,7 +1,15 @@
 from __future__ import absolute_import
-
 from celery import shared_task
+from celery.decorators import task
+import time
+
 
 @shared_task
 def test(param):
-    return 'The test task executed with argument "%s" ' % param
+	time.sleep(20)
+	return 4444444
+
+
+@task(name="sum_two_numbers")
+def add(x, y):
+    return x + y
