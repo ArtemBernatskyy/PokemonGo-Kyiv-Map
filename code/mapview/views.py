d8ec7fb96f2332d9310ed33964569bb8aa87af8b
artembernatskyy <artem.bernatskyy@gmail.com>
2016-07-25 14:19:13 +0300

    maps rendering all right, changed templates

--- code/mapview/views.py
+++ code/mapview/views.py
@@ -1,8 +1,6 @@
 from django.shortcuts import render
-# from .utils.functions import get_pokemons_from_cache
-# from django.views.decorators.cache import cache_page
-from django.core.cache import cache
-# from .tasks import player_scan
+from .utils.functions import get_pokemons_from_cache
+from .tasks import player_scan
 from .models import Player, City
 
 
@@ -17,24 +15,14 @@ from .models import Player, City
 # 	print('::::One more task')
 
 
-# @cache_page(15)
+
 def polygon(request):
-	pokemons_list = []
-	name_list = []
-	pokemons_in_cache = cache.iter_keys("pokemon_*")
-	for pokemon_in_cache in pokemons_in_cache:
-		name_list.append(str(pokemon_in_cache))
-		# pokemons_list.append(cache.get(str(pokemon_in_cache)))
-	temp_list = cache.get_many(name_list)
-
-	for key, pokemon_data in temp_list.items():
-		pokemon_data.update({'image': '/static/mapview/icons/{0}.png'.format(str(pokemon_data['pokemon_id']))})
-
-		pokemons_list.append(pokemon_data)
-	pokemons = pokemons_list
+	pokemons = get_pokemons_from_cache()
 
 	context = {
 		'pokemons': pokemons,
+		# 'cities': City.objects.active(),
+		# 'players': Player.objects.active(),
 	}
 
 	return render(request, "mapview/map.html", context)
